7c0e21917e050a66582c610e1b7f4f655a857146
Philip <molo4nik11@gmail.com>
2014-04-21 02:00:02 +0400

    аа

--- public/javascripts/controllers/filmsCtrl.js
+++ public/javascripts/controllers/filmsCtrl.js
@@ -1,6 +1,35 @@
 app.controller('filmsCtrl', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
 	console.log($scope)
 
+	var width = $(window).width();
+	if (width > 753 && width < 977) {
+		$scope.cutNumber = 250;
+	}
+	if (width > 977 && width < 1185) {
+		$scope.cutNumber = 350;
+	}
+	if (width > 1185) {
+		$scope.cutNumber = 450;
+	}
+
+	$(window).resize(function() {
+		console.log(';;')
+		a = $(this).width();
+		console.log(a)
+		if (a > 753 && a < 977) {
+			$scope.cutNumber = 250;
+		}
+		if (a > 977 && a < 1185) {
+			$scope.cutNumber = 350;
+		}
+		if (a > 1185) {
+			$scope.cutNumber = 450;
+		}
+		console.log($scope.cutNumber);
+		$scope.$apply();
+	})
+
+
 	$scope.selectOptions = {
 		'documental' : 'Документальный',
 		'animation' : 'Анимационный',
